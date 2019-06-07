c2cc32a01760d58ac2023a91f685519bcb8c8ae7
hatlonely <hatlonely@gmail.com>
2019-06-07 19:33:49 +0800

    update

--- internal/account/register.go
+++ internal/account/register.go
@@ -1,21 +1,79 @@
 package account
 
 import (
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/hatlonely/account/internal/mysqldb"
+	"github.com/sirupsen/logrus"
+	"net/http"
 )
 
-func (s *Service) Register(c *gin.Context) {
-
+type RegisterReqBody struct {
+	Username  string `json:"username,omitempty"`
+	Telephone string `json:"telephone,omitempty"`
+	Email     string `json:"email,omitempty"`
+	Password  string `json:"password,omitempty"`
 }
 
-func (s *Service) register(username string, telephone string, email string, password string) (bool, error) {
+type RegisterResBody struct {
+	Success bool `json:"success,omitempty"`
+}
+
+func (s *Service) Register(c *gin.Context) {
+	rid := c.DefaultQuery("rid", NewToken())
+	req := &RegisterReqBody{}
+	var res *RegisterResBody
+	var err error
+	var buf []byte
+	status := http.StatusOK
+
+	defer func() {
+		AccessLog.WithFields(logrus.Fields{
+			"host":   c.Request.Host,
+			"body":   string(buf),
+			"url":    c.Request.URL.String(),
+			"req":    req,
+			"res":    res,
+			"rid":    rid,
+			"err":    err,
+			"status": status,
+		}).Info()
+	}()
+
+	buf, err = c.GetRawData()
+	if err != nil {
+		WarnLog.WithField("@rid", rid).WithField("err", err).Warn("get raw data failed")
+		status = http.StatusBadRequest
+		c.String(status, "")
+		return
+	}
+
+	if err = json.Unmarshal(buf, req); err != nil {
+		WarnLog.WithField("@rid", rid).WithField("err", err).Warn("decode request body failed")
+		status = http.StatusBadRequest
+		c.String(status, "")
+		return
+	}
+
+	res, err = s.register(req)
+	if err != nil {
+		WarnLog.WithField("@rid", rid).WithField("err", err).Warn("login failed")
+		status = http.StatusInternalServerError
+		c.String(status, err.Error())
+		return
+	}
+
+	status = http.StatusOK
+	c.JSON(status, res)
+}
+
+func (s *Service) register(req *RegisterReqBody) (*RegisterResBody, error) {
 	ok, err := s.db.InsertAccount(&mysqldb.Account{
-		Username:  username,
-		Telephone: telephone,
-		Email:     email,
-		Password:  password,
+		Username:  req.Username,
+		Telephone: req.Telephone,
+		Email:     req.Email,
+		Password:  req.Password,
 	})
 
-	return ok, err
+	return &RegisterResBody{Success: ok}, err
 }
