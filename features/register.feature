6057ad8986b3b752f4e914a9923df0e000b3a4d6
hatlonely <hatlonely@gmail.com>
2019-06-09 23:52:31 +0800

    update

--- features/register.feature
+++ features/register.feature
@@ -8,13 +8,13 @@ Feature: register 注册测试
         Then 检查 mysqldb.accounts，存在记录 username: "<username>", telephone: "<telephone>", email: "<email>", password: "<password>"
         Examples:
             | username   | telephone   | email                  | password                         | status | success |
-            | hatlonely1 | 12345678901 | hatlonely1@foxmail.com | e010597fcf126d58fdfa36e636f8fc9e | 200    | true    |
+            | hatlonely1 | 13145678901 | hatlonely1@foxmail.com | e010597fcf126d58fdfa36e636f8fc9e | 200    | true    |
             | hatlonely2 | N/A         | hatlonely2@foxmail.com | 0fe808594e47df1a336bafd8ab32f326 | 200    | true    |
-            | hatlonely3 | 12345678903 | N/A                    | de9baf2c5dde96f0a8b371117e936d4b | 200    | true    |
+            | hatlonely3 | 13145678903 | N/A                    | de9baf2c5dde96f0a8b371117e936d4b | 200    | true    |
 
     Scenario: 重复注册
         Given mysqldb.accounts 删除用户 username: "<username>"
-        Given mysqldb.accounts 创建用户 username: "hatlonely", telephone: "12345678901", email: "hatlonely@foxmail.com", password: "e010597fcf126d58fdfa36e636f8fc9e"
-        When 请求 /register, username: "hatlonely", telephone: "12345678901", email: "hatlonely@foxmail.com", password: "e010597fcf126d58fdfa36e636f8fc9e"
+        Given mysqldb.accounts 创建用户 username: "hatlonely1", telephone: "13145678901", email: "hatlonely1@foxmail.com", password: "e010597fcf126d58fdfa36e636f8fc9e"
+        When 请求 /register, username: "hatlonely1", telephone: "13145678901", email: "hatlonely1@foxmail.com", password: "e010597fcf126d58fdfa36e636f8fc9e"
         Then 检查状态码 res.status_code: 500
-        Then 检查返回包体 res.body，包含字符串 "username [hatlonely] is already exists"
+        Then 检查返回包体 res.body，包含字符串 "username [hatlonely1] is already exists"
--- internal/mysqldb/account_test.go
+++ internal/mysqldb/account_test.go
@@ -12,14 +12,14 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 		So(err, ShouldBeNil)
 		So(m, ShouldNotBeNil)
 
-		m.db.Where("username='hatlonely'").
-			Or("email='hatlonely@foxmail.com'").
-			Or("telephone='+8612345678901'").Delete(&Account{})
+		m.db.Where("username='hatlonely1'").
+			Or("email='hatlonely1@foxmail.com'").
+			Or("telephone='13145678901'").Delete(&Account{})
 
 		Convey("insert account without username", func() {
 			ok, err := m.InsertAccount(&Account{
-				Email:     "hatlonely@foxmail.com",
-				Telephone: "+8612345678901",
+				Email:     "hatlonely1@foxmail.com",
+				Telephone: "13145678901",
 				Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 			})
 			So(err, ShouldNotBeNil)
@@ -28,7 +28,7 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 
 		Convey("insert account without email and telephone", func() {
 			ok, err := m.InsertAccount(&Account{
-				Username: "hatlonely",
+				Username: "hatlonely1",
 				Password: "e010597fcf126d58fdfa36e636f8fc9e",
 			})
 			So(err, ShouldNotBeNil)
@@ -37,61 +37,61 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 
 		Convey("insert account with username and email", func() {
 			ok, err := m.InsertAccount(&Account{
-				Username: "hatlonely",
-				Email:    "hatlonely@foxmail.com",
+				Username: "hatlonely1",
+				Email:    "hatlonely1@foxmail.com",
 				Password: "e010597fcf126d58fdfa36e636f8fc9e",
 			})
 			So(err, ShouldBeNil)
 			So(ok, ShouldBeTrue)
 
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely1")
 			So(err, ShouldBeNil)
-			So(account.Username, ShouldEqual, "hatlonely")
-			So(account.Email, ShouldEqual, "hatlonely@foxmail.com")
+			So(account.Username, ShouldEqual, "hatlonely1")
+			So(account.Email, ShouldEqual, "hatlonely1@foxmail.com")
 			So(account.Telephone, ShouldEqual, "")
 			So(account.Password, ShouldEqual, "e010597fcf126d58fdfa36e636f8fc9e")
 		})
 
 		Convey("insert account with username and telephone", func() {
 			ok, err := m.InsertAccount(&Account{
-				Username:  "hatlonely",
-				Telephone: "+8612345678901",
+				Username:  "hatlonely1",
+				Telephone: "13145678901",
 				Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 			})
 			So(err, ShouldBeNil)
 			So(ok, ShouldBeTrue)
 
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely1")
 			So(err, ShouldBeNil)
-			So(account.Username, ShouldEqual, "hatlonely")
+			So(account.Username, ShouldEqual, "hatlonely1")
 			So(account.Email, ShouldEqual, "")
-			So(account.Telephone, ShouldEqual, "+8612345678901")
+			So(account.Telephone, ShouldEqual, "13145678901")
 			So(account.Password, ShouldEqual, "e010597fcf126d58fdfa36e636f8fc9e")
 		})
 
 		Convey("insert account with username and telephone and email", func() {
 			ok, err := m.InsertAccount(&Account{
-				Username:  "hatlonely",
-				Email:     "hatlonely@foxmail.com",
-				Telephone: "+8612345678901",
+				Username:  "hatlonely1",
+				Email:     "hatlonely1@foxmail.com",
+				Telephone: "13145678901",
 				Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 			})
 			So(err, ShouldBeNil)
 			So(ok, ShouldBeTrue)
 
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely1")
 			So(err, ShouldBeNil)
-			So(account.Username, ShouldEqual, "hatlonely")
-			So(account.Email, ShouldEqual, "hatlonely@foxmail.com")
-			So(account.Telephone, ShouldEqual, "+8612345678901")
+			So(account.Username, ShouldEqual, "hatlonely1")
+			So(account.Email, ShouldEqual, "hatlonely1@foxmail.com")
+			So(account.Telephone, ShouldEqual, "13145678901")
 			So(account.Password, ShouldEqual, "e010597fcf126d58fdfa36e636f8fc9e")
 		})
 
 		Convey("insert account dup usename or email or telephone", func() {
 			ok, err := m.InsertAccount(&Account{
-				Username:  "hatlonely",
-				Email:     "hatlonely@foxmail.com",
-				Telephone: "+8612345678901",
+				Username:  "hatlonely1",
+				Email:     "hatlonely1@foxmail.com",
+				Telephone: "13145678901",
 				Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 			})
 			So(err, ShouldBeNil)
@@ -99,9 +99,9 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 
 			Convey("insert account dup username", func() {
 				ok, err = m.InsertAccount(&Account{
-					Username:  "hatlonely",
-					Email:     "hatlonely1@foxmail.com",
-					Telephone: "+8612345678902",
+					Username:  "hatlonely1",
+					Email:     "hatlonely12@foxmail.com",
+					Telephone: "13145678902",
 					Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 				})
 				So(err, ShouldNotBeNil)
@@ -110,9 +110,9 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 
 			Convey("insert account dup telephone", func() {
 				ok, err = m.InsertAccount(&Account{
-					Username:  "hatlonely1",
-					Email:     "hatlonely1@foxmail.com",
-					Telephone: "+8612345678901",
+					Username:  "hatlonely12",
+					Email:     "hatlonely12@foxmail.com",
+					Telephone: "13145678901",
 					Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 				})
 				So(err, ShouldNotBeNil)
@@ -121,8 +121,8 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 
 			Convey("insert account dup email", func() {
 				ok, err = m.InsertAccount(&Account{
-					Username:  "hatlonely1",
-					Email:     "hatlonely@foxmail.com",
+					Username:  "hatlonely12",
+					Email:     "hatlonely1@foxmail.com",
 					Telephone: "+8612345678902",
 					Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 				})
@@ -134,14 +134,14 @@ func TestMysqlDB_InsertAccount(t *testing.T) {
 }
 
 func TestMysqlDB_SelectAccountByUsernameOrTelephoneOrEmail(t *testing.T) {
-	m, err := NewMysqlDB("hatlonely:keaiduo1@/hads?charset=utf8&parseTime=True&loc=Local")
+	m, err := NewMysqlDB("hatlonely1:keaiduo1@/hads?charset=utf8&parseTime=True&loc=Local")
 	Convey("test mysqldb select account by username or telephone or email", t, func() {
 		So(err, ShouldBeNil)
 		So(m, ShouldNotBeNil)
 
-		m.db.Where("username='hatlonely'").
-			Or("email='hatlonely@foxmail.com'").
-			Or("telephone='+8612345678901'").Delete(&Account{})
+		m.db.Where("username='hatlonely1'").
+			Or("email='hatlonely1@foxmail.com'").
+			Or("telephone='13145678901'").Delete(&Account{})
 
 		Convey("select account use empty key", func() {
 			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("")
@@ -150,44 +150,44 @@ func TestMysqlDB_SelectAccountByUsernameOrTelephoneOrEmail(t *testing.T) {
 		})
 
 		Convey("select account use nonexists key", func() {
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely1")
 			So(account, ShouldBeNil)
 			So(err, ShouldBeNil)
 		})
 
 		ok, err := m.InsertAccount(&Account{
-			Username:  "hatlonely",
-			Email:     "hatlonely@foxmail.com",
-			Telephone: "+8612345678901",
+			Username:  "hatlonely1",
+			Email:     "hatlonely1@foxmail.com",
+			Telephone: "13145678901",
 			Password:  "e010597fcf126d58fdfa36e636f8fc9e",
 		})
 		So(err, ShouldBeNil)
 		So(ok, ShouldBeTrue)
 
 		Convey("select account by username", func() {
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely1")
 			So(err, ShouldBeNil)
-			So(account.Username, ShouldEqual, "hatlonely")
-			So(account.Email, ShouldEqual, "hatlonely@foxmail.com")
-			So(account.Telephone, ShouldEqual, "+8612345678901")
+			So(account.Username, ShouldEqual, "hatlonely1")
+			So(account.Email, ShouldEqual, "hatlonely1@foxmail.com")
+			So(account.Telephone, ShouldEqual, "13145678901")
 			So(account.Password, ShouldEqual, "e010597fcf126d58fdfa36e636f8fc9e")
 		})
 
 		Convey("select account by telephone", func() {
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely@foxmail.com")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("hatlonely1@foxmail.com")
 			So(err, ShouldBeNil)
-			So(account.Username, ShouldEqual, "hatlonely")
-			So(account.Email, ShouldEqual, "hatlonely@foxmail.com")
-			So(account.Telephone, ShouldEqual, "+8612345678901")
+			So(account.Username, ShouldEqual, "hatlonely1")
+			So(account.Email, ShouldEqual, "hatlonely1@foxmail.com")
+			So(account.Telephone, ShouldEqual, "13145678901")
 			So(account.Password, ShouldEqual, "e010597fcf126d58fdfa36e636f8fc9e")
 		})
 
 		Convey("select account by email", func() {
-			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("+8612345678901")
+			account, err := m.SelectAccountByUsernameOrTelephoneOrEmail("13145678901")
 			So(err, ShouldBeNil)
-			So(account.Username, ShouldEqual, "hatlonely")
-			So(account.Email, ShouldEqual, "hatlonely@foxmail.com")
-			So(account.Telephone, ShouldEqual, "+8612345678901")
+			So(account.Username, ShouldEqual, "hatlonely1")
+			So(account.Email, ShouldEqual, "hatlonely1@foxmail.com")
+			So(account.Telephone, ShouldEqual, "13145678901")
 			So(account.Password, ShouldEqual, "e010597fcf126d58fdfa36e636f8fc9e")
 		})
 	})
